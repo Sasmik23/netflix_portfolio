72fdb53407fbf50b944a27966bb73aea50290381
Sasmik23 <mikhilanand@gmail.com>
2025-11-24 14:03:18 +0800

    Convert images to .jpg and remove _headers file

--- src/browse/browse.tsx
+++ src/browse/browse.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import ProfileCard from '../components/ProfileCard';
-import clark_cs from '../images/Clark_Kent_CS.jpeg';
-import clark_theatre from '../images/Clark_Kent_Theatre.jpeg';
+import clark_cs from '../images/Clark_Kent_CS.jpg';
+import clark_theatre from '../images/Clark_Kent_Theatre.jpg';
 import { profileBackgroundGifs, getRandomGif } from '../data/profileGifs';
 import './browse.css';
 
